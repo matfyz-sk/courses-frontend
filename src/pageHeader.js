0664f48bf6f13ad39464670aacf54753786cfed0
adam.grund@gmail.com <adam.grund@gmail.com>
2020-02-04 21:50:37 +0100

    add responsivnes to PageHeader

--- src/pageHeader.js
+++ src/pageHeader.js
@@ -1,55 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-Navbar,
-NavbarBrand,
-NavLink,
-NavItem,
-DropdownMenu,
-DropdownItem,
-DropdownToggle,
-Nav,
-UncontrolledDropdown
-} from 'reactstrap';
+  Navbar,
+  NavbarBrand,
+  NavLink,
+  NavItem,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  Nav,
+  UncontrolledDropdown,
+  NavbarToggler,
+  Collapse
+} from "reactstrap";
 import { NavLink as NV } from "react-router-dom";
 import { connect } from "react-redux";
-import { setUserAdmin } from './redux/actions';
+import { setUserAdmin } from "./redux/actions";
 
-class PageHeader extends Component{
-  render(){
-    return(
+class PageHeader extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    return (
       <Navbar color="light" light expand="md">
         <UncontrolledDropdown>
           <DropdownToggle>
-            <NavbarBrand onClick={()=>this.props.history.push('/')}>Webdesign</NavbarBrand>
+            <NavbarBrand href="/">Webdesign</NavbarBrand>
           </DropdownToggle>
           <DropdownMenu>
             Webdesign
           </DropdownMenu>
         </UncontrolledDropdown>
-
-        <Nav navbar>
-          <NavItem>
-            <NavLink className="clickable" tag={NV} to="/info">Info</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="clickable" tag={NV} to="/lectures">Lectures</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="clickable" tag={NV} to="/labs">Labs</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="clickable" tag={NV} to="/assignments">Assignments</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="clickable" tag={NV} to="/results">Results</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="clickable" tag={NV} to="/quiz">Quiz</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="clickable" tag={NV} to="/files">Files</NavLink>
-          </NavItem>
-        </Nav>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink tag={NV} to="/info">
+                Info
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={NV} to="/lectures">
+                Lectures
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={NV} to="/labs">
+                Labs
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={NV} to="/assignments">
+                Assignments
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={NV} to="/results">
+                Results
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={NV} to="/quiz">
+                Quiz
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={NV} to="/files">
+                Files
+              </NavLink>
+            </NavItem>
+          </Nav>
           <Nav className="ml-auto" navbar>
             <NavItem>
               <NavLink>
@@ -58,33 +85,32 @@ class PageHeader extends Component{
             </NavItem>
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
-                {this.props.isAdmin?'Admin':'Student'}
+                {this.props.isAdmin ? "Admin" : "Student"}
               </DropdownToggle>
               <DropdownMenu right>
-                <DropdownItem>
-                  Account settings
-                </DropdownItem>
-                <DropdownItem className="clickable" onClick={()=>this.props.setUserAdmin(!this.props.isAdmin)}>
-                  Change to {this.props.isAdmin?'Student':'Admin'}
+                <DropdownItem>Account settings</DropdownItem>
+                <DropdownItem
+                  className="clickable"
+                  onClick={() => this.props.setUserAdmin(!this.props.isAdmin)}
+                >
+                  Change to {this.props.isAdmin ? "Student" : "Admin"}
                 </DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem>
-                  Log out
-                </DropdownItem>
+                <DropdownItem>Log out</DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
+        </Collapse>
       </Navbar>
-
-    )
+    );
   }
 }
 
 const mapStateToProps = ({ userReducer }) => {
-	const { isAdmin } = userReducer;
-	return {
+  const { isAdmin } = userReducer;
+  return {
     isAdmin
-	};
+  };
 };
 
 export default connect(mapStateToProps, { setUserAdmin })(PageHeader);
