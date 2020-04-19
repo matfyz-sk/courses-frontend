eb820b98d399d98cbab06dd9d2197b97203b9147
adam.grund@gmail.com <adam.grund@gmail.com>
2020-04-19 15:45:26 +0200

    export API_URL

--- src/configuration/api.js
+++ src/configuration/api.js
@@ -2,9 +2,10 @@ export const BACKEND_URL =
   process.env.NODE_ENV === 'development'
     ? 'https://courses.matfyz.sk:4440' // TESTING
     : 'https://courses.matfyz.sk:4440'
+//  'https://courses.matfyz.sk:4440'
 // 'https://courses.matfyz.sk:4441' // PRODUCTION PLS DON'T use this server, only when deploying
 // 'http://localhost:3010' // Local
 
-const API_URL = `${BACKEND_URL}/data`
+export const API_URL = `${BACKEND_URL}/data`
 
 export default { API_URL }
