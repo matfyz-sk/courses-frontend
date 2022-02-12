e1b5372c4320e27a5948100f7aa7245848757ed4
Rastislav Urbanek <raleu1@gmail.com>
2022-02-13 00:38:49 +0100

    added enums of document subclasses + gitignore

---
+++ src/pages/documents/enums/document-enums.jsx
@@ -0,0 +1,23 @@
+export const DocumentEnums = Object.freeze({
+    internalDocument: {
+        id: 'http://www.courses.matfyz.sk/ontology#InternalDocument',
+        entityName: 'internalDocument',
+        name: 'Internal Document',
+    },
+    externalDocument: {
+        id: 'http://www.courses.matfyz.sk/ontology#ExternalDocument',
+        entityName: 'externalDocument',
+        name: 'External Document',
+    },
+    file: {
+        id: 'http://www.courses.matfyz.sk/ontology#File',
+        entityName: 'file',
+        name: 'File',
+    },
+    material: {
+        id: 'http://www.courses.matfyz.sk/ontology#Material',
+        entityName: 'material',
+        name: 'Material',
+    }
+})
+  
