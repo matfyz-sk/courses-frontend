9ff91e727b67981abb31c9f7215dd6f4bc78ec5b
bl96 <47901930+bl96@users.noreply.github.com>
2022-03-11 16:45:25 +0100

    update api

--- src/configuration/api.js
+++ src/configuration/api.js
@@ -1,8 +1,6 @@
 export const BACKEND_URL =
   process.env.REACT_APP_BACKEND_URL ?
     process.env.REACT_APP_BACKEND_URL :
-    'https://courses.matfyz.sk:4440'
+    'https://courses.matfyz.sk:4440';
 
-export const API_URL = `${BACKEND_URL}/data`
-
-export default {API_URL}
+export const API_URL = `${BACKEND_URL}/data`;
