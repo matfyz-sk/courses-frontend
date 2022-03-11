156c918ce7de48bca195ec1348d1382c9a5e66b6
bl96 <47901930+bl96@users.noreply.github.com>
2022-03-11 16:28:07 +0100

    Update api.js

--- src/configuration/api.js
+++ src/configuration/api.js
@@ -1,18 +1,8 @@
 export const BACKEND_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'https://courses.matfyz.sk:4440'
-    : 'https://courses.matfyz.sk:4440'
-
-// DEV apache forward this to matfyz.sk:3010
-// 'https://courses.matfyz.sk:4440'
-
-// PRODUCTION apache forward this to matfyz.sk:3011
-// PLS DON'T use this server, only when deploying
-// 'https://courses.matfyz.sk:4441'
-
-// LOCAL
-// 'http://localhost:3010'
+  process.env.REACT_APP_BACKEND_URL ?
+    process.env.REACT_APP_BACKEND_URL :
+    'https://courses.matfyz.sk:4440'
 
 export const API_URL = `${BACKEND_URL}/data`
 
-export default { API_URL }
+export default {API_URL}
