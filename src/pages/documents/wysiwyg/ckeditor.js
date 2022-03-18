244ae32fed33cc7a811a4b1fea9783cb2f5e6f37
Rasto <raleu1@gmail.com>
2022-03-18 23:51:44 +0100

    + readonly mode, mediaEmbed saves iframe data in html

--- src/pages/documents/wysiwyg/ckeditor.js
+++ src/pages/documents/wysiwyg/ckeditor.js
@@ -2,17 +2,67 @@ import React from 'react'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import * as CKSUPEREDITOR from 'courses-wysiwyg-superbuild'
 
-function CustomEditor({ content, setContent, mimeType }) {
-  //FIXME saving in source mode doesn't save it
-  //TODO clean code
+function CustomEditor({
+  content,
+  setContent,
+  mimeType,
+  isReadOnly
+}) {
+  // TODO clean code
+  // TODO upgrade superbuild dep.
   return (
     <>
+      {/* <CKEditor
+        onReady={editor => {
+          if (mimeType === 'text/html') {
+            window.editor1 = editor
+          } else {
+            window.editor2 = editor
+          }
+          // set
+          editor.editing.view.change(writer => {
+            writer.setStyle(
+              'min-height',
+              '350px',
+              editor.editing.view.document.getRoot()
+            )
+          })
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData()
+          setContent(data)
+        }}
+        // onBlur={ ( event, editor ) => {
+        // } }
+        // onFocus={ ( event, editor ) => {
+        // } }
+        editor={mimeType === 'text/html' ? CKSUPEREDITOR.HTMLClassicEditor : CKSUPEREDITOR.MarkdownClassicEditor}
+        data={content}
+      /> */}
       {mimeType === 'text/html' && (
         <CKEditor
+          config={{
+            htmlSupport: {
+              allow: [
+                {
+                  // TODO maybe don't allow everything HAHa
+                  name: /.*/,
+                  attributes: true,
+                  classes: true,
+                  styles: true,
+                },
+              ],
+              disallow: [
+                /* HTML features to disallow */
+              ],
+            },
+            mediaEmbed: {
+              previewsInData:true
+            },
+          }}
           onReady={editor => {
-            // console.log( 'Editor is ready to use!', editor );
             window.editor1 = editor
-            // set
+
             editor.editing.view.change(writer => {
               writer.setStyle(
                 'min-height',
@@ -20,25 +70,48 @@ function CustomEditor({ content, setContent, mimeType }) {
                 editor.editing.view.document.getRoot()
               )
             })
+
+            const toolbarElement = editor.ui.view.toolbar.element
+
+            if (isReadOnly) {
+              editor.isReadOnly = true
+              toolbarElement.style.display = 'none'
+            } else {
+              editor.isReadOnly = false
+              toolbarElement.style.display = 'flex'
+            }
           }}
           onChange={(event, editor) => {
-            const data = editor.getData()
-            setContent(data)
+            if (!isReadOnly) {
+              const data = editor.getData()
+              setContent(data)
+            }
           }}
-          // onBlur={ ( event, editor ) => {
-          // } }
-          // onFocus={ ( event, editor ) => {
-          // } }
           editor={CKSUPEREDITOR.HTMLClassicEditor}
           data={content}
         />
       )}
       {mimeType === 'text/markdown' && (
         <CKEditor
+          config={{
+            htmlSupport: {
+              allow: [
+                {
+                  // TODO maybe don't allow everything HAHa
+                  name: /.*/,
+                  attributes: true,
+                  classes: true,
+                  styles: true,
+                },
+              ],
+              disallow: [
+                /* HTML features to disallow */
+              ],
+            }
+          }}
           onReady={editor => {
-            // console.log( 'Editor is ready to use!', editor );
             window.editor2 = editor
-            // set
+
             editor.editing.view.change(writer => {
               writer.setStyle(
                 'min-height',
@@ -46,15 +119,22 @@ function CustomEditor({ content, setContent, mimeType }) {
                 editor.editing.view.document.getRoot()
               )
             })
+            const toolbarElement = editor.ui.view.toolbar.element
+
+            if (isReadOnly) {
+              editor.isReadOnly = true
+              toolbarElement.style.display = 'none'
+            } else {
+              editor.isReadOnly = false
+              toolbarElement.style.display = 'flex'
+            }
           }}
           onChange={(event, editor) => {
-            const data = editor.getData()
-            setContent(data)
+            if (!isReadOnly) {
+              const data = editor.getData()
+              setContent(data)
+            }
           }}
-          // onBlur={ ( event, editor ) => {
-          // } }
-          // onFocus={ ( event, editor ) => {
-          // } }
           editor={CKSUPEREDITOR.MarkdownClassicEditor}
           data={content}
         />
