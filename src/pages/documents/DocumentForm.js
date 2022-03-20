9c260aed3269948a608f016e68818c676c1f1110
Rasto <raleu1@gmail.com>
2022-03-20 01:40:31 +0100

    refactor

--- src/pages/documents/DocumentForm.js
+++ src/pages/documents/DocumentForm.js
@@ -31,6 +31,9 @@ import * as ROUTES from '../../constants/routes'
 import { redirect } from '../../constants/redirect'
 import Page404 from 'pages/errors/Page404'
 import { isValidHttpUrl } from '../../functions/validators'
+import editDocument  from './functions/documentCreation'
+import downloadBase64File from './functions/downloadBase64File'
+
 
 //TODO add conventional styling
 const ICON_SIZE = 56
@@ -50,6 +53,7 @@ const PAYLOAD_ENTITIES = [
 function DocumentForm(props) {
   // FIXME ?? operator and ?. compatibility
   // FIXME large base64 file uploads not working
+  // FIXME only owner can do
 
   const [status, setStatus] = useState(200)
 
@@ -143,177 +147,35 @@ function DocumentForm(props) {
     if (!loadingCourseInstance && !loadingDocument) setLoading(false)
   }, [loadingCourseInstance, loadingDocument])
 
-  const canCreatePayload = () => {
-    return PAYLOAD_ENTITIES.includes(entityName)
-  }
-
-  const createNewVersionData = async () => {
-    // destructuring previous version
-    const {
-      '@id': previousVersionId,
-      '@type': type,
-      createdAt,
-      createdBy,
-      nextVersion,
-      ...properties
-    } = document
-
-    if (canCreatePayload()) {
-      if (entityName === DocumentEnums.file.entityName) {
-        var payload = {
-          content,
-          // content: `""${content}""`, // needed for sparql/rdf
-        }
-      } else {
-        var payload = {
-          content: `""${content}""`, // needed for sparql/rdf
-        }
-      }
-      console.log({ payload })
-      var payloadId = await axiosAddEntity(payload, 'payload').then(
-        response => {
-          if (response.failed) {
-            console.error('payload', response.error)
-            setStatus(response.response ? response.response.status : 500)
-            return
-          }
-          return getIRIFromAddResponse(response)
-        }
-      )
-      console.log(payloadId)
-    }
-
-    let newVersion = {
-      ...properties,
-      _type: entityName,
-      name,
-      isDeleted: false,
-      author: [props.user.fullURI], //? idk if we are adding a new author, if its a collaborative scenario?...
-      owner: props.user.fullURI,
-      courseInstance: [props.courseInstance['@id']], // TODO push to existing ones
-    }
-    // add addtional params
-    switch (entityName) {
-      case DocumentEnums.internalDocument.entityName:
-        console.log({ payloadId })
-        const internalDocumentParams = {
-          payload: payloadId,
-          mimeType,
-        }
-        newVersion = {
-          ...newVersion,
-          ...internalDocumentParams,
-        }
-        break
-      case DocumentEnums.externalDocument.entityName:
-        const externalDocumentParams = { uri }
-        newVersion = {
-          ...newVersion,
-          ...externalDocumentParams,
-        }
-        break
-      case DocumentEnums.file.entityName:
-        const fileParams = {
-          payload: payloadId,
-          filename,
-          mimeType,
-        }
-        newVersion = {
-          ...newVersion,
-          ...fileParams,
-        }
-        break
-      default:
-        break
-    }
-
-    if (isInEditingMode()) {
-      newVersion = {
-        ...newVersion,
-        previousVersion: previousVersionId,
-        historicVersion: [
-          previousVersionId,
-          ...document.historicVersion.map(doc => doc['@id']),
-        ],
-      }
-    }
-    return newVersion
-  }
-
-  const createNewVersion = async newVersion => {
-    console.log({ newVersion })
-    return await axiosAddEntity(newVersion, 'document').then(response => {
-      if (response.failed) {
-        console.error(response.error)
-        console.log('creating new ver')
-        setStatus(response.response ? response.response.status : 500)
-        return
-      }
-      return getIRIFromAddResponse(response)
-    })
-  }
-
-  const setSuccessorOfOldVersion = successorId => {
-    const entityUrl = `${entityName}/${id}`
-    const dataToUpdate = {
-      nextVersion: successorId,
-    }
-
-    axiosUpdateEntity(dataToUpdate, entityUrl).then(response => {
-      if (response.failed) {
-        console.error(response.error)
-        console.log('setting old version')
-        setStatus(response.response ? response.response.status : 500)
-      }
-    })
-  }
-
-  const replaceInCurrentDocuments = (newVersionId, oldVersionId) => {
-    // ? works as intended when oldVersionId is undefined... but maybe still add a condition?
-      const newCurrentDocuments = {
-        hasDocument: [
-          newVersionId,
-          ...props.courseInstance.hasDocument
-            .map(doc => doc['@id'])
-            .filter(id => id !== oldVersionId),
-        ],
-      }
-    // const newCurrentDocuments = {
-    //     hasDocument: []
-    // }
-    const entityUrl = `courseInstance/${courseId}`
-
-    axiosUpdateEntity(newCurrentDocuments, entityUrl).then(response => {
-      if (response.failed) {
-        console.log('update ci')
-        console.error(response.error)
-        setStatus(response.response ? response.response.status : 500)
-        return
-      }
-      props.setCurrentDocumentsOfCourseInstance(
-        newCurrentDocuments.hasDocument.map(doc => ({ '@id': doc }))
-      )
-      props.history.push(
-        redirect(ROUTES.DOCUMENTS, [{ key: 'course_id', value: courseId }])
-      )
-    })
-  }
 
   const handleEdit = async e => {
     e.preventDefault()
     if (!formValid()) {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
       return
     }
-    const data = await createNewVersionData()
-    const newVersionId = await createNewVersion(data)
+    console.log({props})
+    const editProps = {
+      courseId,
+      entityName,
+      setStatus,
+      isInEditingMode: isInEditingMode(),
+      ...props,
+    }
+    const newVersionId = await editDocument(
+      { name, mimeType, uri, filename, payload: [{ content }] },
+      document,
+      editProps
+    )
     if (!newVersionId) {
-      console.error('Editing was unsuccessful!')
+      setStatus(500)
       return
     }
-    if (isInEditingMode()) {
-      setSuccessorOfOldVersion(newVersionId)
-    }
-    replaceInCurrentDocuments(newVersionId, document['@id'])
+    props.history.push(
+      redirect(ROUTES.DOCUMENTS, [
+        { key: 'course_id', value: courseId },
+      ])
+    )
   }
 
   const mimeTypeOptions = ['text/html', 'text/markdown']
@@ -334,17 +196,8 @@ function DocumentForm(props) {
   }
 
   const onDownloadFile = async e => {
-    const decodedFile = await base64dataToFile(content, filename, mimeType)
-    const href = URL.createObjectURL(decodedFile)
-    const a = Object.assign(window.document.createElement('a'), {
-      href,
-      style: 'display: none',
-      download: filename,
-    })
-    window.document.body.appendChild(a)
-    a.click()
-    URL.revokeObjectURL(href)
-    a.remove()
+    e.preventDefault()
+    downloadBase64File({mimeType, filename, payload: [{content}]}, window)
   }
 
   const formValid = () => {
