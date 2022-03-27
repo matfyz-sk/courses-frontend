055e2f0c60953ede715606dd6036b597bf1447d6
Rasto <raleu1@gmail.com>
2022-03-27 20:08:03 +0200

    styling harmonization + integrated material relations ui

--- src/pages/documents/DocumentForm.js
+++ src/pages/documents/DocumentForm.js
@@ -1,4 +1,17 @@
-import { MenuItem, Select } from '@material-ui/core'
+import {
+  Box,
+  Button,
+  MenuItem,
+  Select,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Checkbox,
+  TextField,
+  ThemeProvider,
+  InputLabel,
+  Input,
+} from '@material-ui/core'
 import React, { useState, useEffect, useCallback } from 'react'
 import { withRouter } from 'react-router'
 import { Link } from 'react-router-dom'
@@ -8,17 +21,7 @@ import {
   getResponseBody,
   fileToBase64,
 } from 'helperFunctions'
-
-import {
-  Alert,
-  Button,
-  CustomInput,
-  Form,
-  FormFeedback,
-  FormGroup,
-  Label,
-  Input,
-} from 'reactstrap'
+import { Alert, Form, Label } from 'reactstrap'
 import { HiDownload } from 'react-icons/hi'
 import CustomEditor from './wysiwyg/ckeditor'
 import { DocumentEnums } from './enums/document-enums'
@@ -27,28 +30,18 @@ import * as ROUTES from '../../constants/routes'
 import { redirect } from '../../constants/redirect'
 import Page404 from '../errors/Page404'
 import { isValidHttpUrl } from '../../functions/validators'
-import editDocument  from './functions/documentCreation'
+import editDocument from './functions/documentCreation'
 import downloadBase64File from './functions/downloadBase64File'
+import MaterialForm from './MaterialForm'
+import { customTheme, useStyles } from './styles/styles'
 
-
-//TODO add conventional styling
-const ICON_SIZE = 56
-const ICON_COLOR = '#237a23'
-
-const customErrorStyle = {
-  marginTop: '.25rem',
-  fontSize: '80%',
-  color: '#dc3545',
-}
-
-const PAYLOAD_ENTITIES = [
-  DocumentEnums.file.entityName,
-  DocumentEnums.internalDocument.entityName,
-]
 
 function DocumentForm(props) {
   // FIXME large base64 file uploads not working
-  // FIXME only owner can do
+  // FIXME only owner can do or see
+  // TODO change the role of courseInstance
+
+  const style = useStyles()
 
   const [status, setStatus] = useState(200)
 
@@ -62,6 +55,9 @@ function DocumentForm(props) {
   const [document, setDocument] = useState({})
   const isInEditingMode = useCallback(() => id !== '', [id])
 
+  const [loadingMaterialRelations, setLoadingMaterialRelations] = useState(
+    false
+  )
   const [loadingCourseInstance, setLoadingCourseInstance] = useState(
     props.courseInstance == null
   )
@@ -70,6 +66,15 @@ function DocumentForm(props) {
 
   // shared among all document subclasses
   const [name, setName] = useState('')
+
+  const [isMaterial, setIsMaterial] = useState(false) // TODO setIsMaterial
+  const [covers, setCovers] = useState([])
+  const [mentions, setMentions] = useState([])
+  const [requires, setRequires] = useState([])
+  const [assumes, setAssumes] = useState([])
+  const [isAlternativeTo, setIsAlternativeTo] = useState([])
+  const [refersTo, setRefersTo] = useState([])
+  const [generalizes, setGeneralizes] = useState([])
 
   // document subclass specific
   const [fileLoaded, setFileLoaded] = useState(false)
@@ -100,6 +105,7 @@ function DocumentForm(props) {
           responseDocument.isDeleted ||
           responseDocument.nextVersion.length !== 0
         ) {
+          // TODO not intended in the past course instances
           props.history.push(
             redirect(ROUTES.DOCUMENTS, [{ key: 'course_id', value: courseId }])
           )
@@ -128,10 +134,12 @@ function DocumentForm(props) {
         }
         setLoadingDocument(false)
       })
-  }, [courseId, isInEditingMode]) // ? shouldn't id be there as well?
+  }, [courseId, isInEditingMode])
+
+  useEffect(() => {}, [props.location.state])
 
   useEffect(() => {
-    fetchDocument()    
+    fetchDocument()
   }, [fetchDocument])
 
   useEffect(() => {
@@ -139,9 +147,9 @@ function DocumentForm(props) {
   }, [props.courseInstance])
 
   useEffect(() => {
-    if (!loadingCourseInstance && !loadingDocument) setLoading(false)
-  }, [loadingCourseInstance, loadingDocument])
-
+    if (!loadingCourseInstance && !loadingDocument && !loadingMaterialRelations)
+      setLoading(false)
+  }, [loadingCourseInstance, loadingDocument, loadingMaterialRelations])
 
   const handleEdit = async e => {
     e.preventDefault()
@@ -149,13 +157,16 @@ function DocumentForm(props) {
       window.scrollTo({ top: 0, behavior: 'smooth' })
       return
     }
-    console.log({props})
+    console.log({ props })
     const editProps = {
       courseId,
       entityName,
       setStatus,
       isInEditingMode: isInEditingMode(),
       ...props,
+    }
+    if (isMaterial) {
+      console.log('implement')
     }
     const newVersionId = await editDocument(
       { name, mimeType, uri, filename, payload: [{ content }] },
@@ -167,16 +178,9 @@ function DocumentForm(props) {
       return
     }
     props.history.push(
-      redirect(ROUTES.DOCUMENTS, [
-        { key: 'course_id', value: courseId },
-      ])
+      redirect(ROUTES.DOCUMENTS, [{ key: 'course_id', value: courseId }])
     )
   }
-
-  const mimeTypeOptions = ['text/html', 'text/markdown']
-
-  // TODO change format selector accordingly
-  // const langModes = { 'text/html': 'html', 'text/markdown': 'markdown' }
 
   const onChangeFile = e => {
     fileToBase64(e.target.files[0]).then(base64Content => {
@@ -192,7 +196,7 @@ function DocumentForm(props) {
 
   const onDownloadFile = async e => {
     e.preventDefault()
-    downloadBase64File({mimeType, filename, payload: [{content}]}, window)
+    downloadBase64File({ mimeType, filename, payload: [{ content }] }, window)
   }
 
   const formValid = () => {
@@ -223,125 +227,195 @@ function DocumentForm(props) {
   }
 
   return (
-    <Form style={{ maxWidth: '1000px', margin: '20px auto' }} onSubmit={handleEdit}>
-      <h1 style={{ marginBottom: '2em' }}>
-        {isInEditingMode() ? 'Edit' : 'Create'} Document
-      </h1>
-      <FormGroup style={{ width: '40%' }}>
-        <Label for="name">Name </Label>
-        <Input
-          id="name"
-          invalid={name.length === 0}
+    <ThemeProvider theme={customTheme}>
+      <Form
+        style={{ maxWidth: '1000px', margin: '20px auto' }}
+        onSubmit={handleEdit}
+      >
+        <h1 style={{ textAlign: 'center', marginBottom: '1em' }}>
+          Document {isInEditingMode() ? 'editing' : 'creation'}
+        </h1>
+        <TextField
+          error={name.length === 0}
+          id="name-textfield"
+          label="Name"
+          type="text"
+          style={{ width: '100%' }}
           value={name}
           onChange={e => setName(e.target.value)}
+          helperText={name.length === 0 ? 'Name is required' : ''}
+          variant="outlined"
         />
-        <FormFeedback>Name is required</FormFeedback>
-      </FormGroup>
-      <br />
-      <br />
-      {entityName === DocumentEnums.externalDocument.entityName && (
-        <FormGroup>
-          <Label for="url"> Url </Label>
-          <Input
-            id="url"
+        <br />
+        <br />
+        {entityName === DocumentEnums.externalDocument.entityName && (
+          <TextField
+            error={!isValidHttpUrl(uri)}
+            id="url-textfield"
+            label="Url"
             type="url"
-            style={{ width: '40%' }}
-            invalid={!isValidHttpUrl(uri)}
+            style={{ width: '100%' }}
             value={uri}
             onChange={e => setUri(e.target.value)}
+            helperText={!isValidHttpUrl(uri) ? 'Valid url is required' : ''}
+            variant="outlined"
           />
-          <FormFeedback>Valid url is required</FormFeedback>
-        </FormGroup>
-      )}
-
-      {entityName === DocumentEnums.file.entityName && isInEditingMode() && (
-        <>
-          <Label for="file-download">
-            Saved file:{' '}
-            <Link
-              id="file-download"
-              to={{ textDecoration: 'none' }}
-              onClick={onDownloadFile}
-            >
-              <HiDownload style={{ color: ICON_COLOR }} size={ICON_SIZE} />
-            </Link>
-          </Label>
-        </>
-      )}
-      {fileLoaded && (
-        <>
-          <p>{filename}</p>
-          <p>{mimeType}</p>
-          <p>{filePath}</p>
-        </>
-      )}
-
-      <FormGroup
-        style={{ width: '40%' }}
-        hidden={entityName !== DocumentEnums.file.entityName}
-      >
-        <Label for="file"> File </Label>
-        <CustomInput
-          id="file"
-          type="file"
-          invalid={filename.length === 0}
-          label={filename || '...'}
-          value={filePath}
-          onChange={onChangeFile}
-        />
-        {filename.length === 0 && (
-          <p style={customErrorStyle}>File is required</p>
         )}
-      </FormGroup>
+        {entityName === DocumentEnums.file.entityName && (
+          <>
+            <Input
+              error={filename.length === 0}
+              id="upload-button-file"
+              type="file"
+              style={{ display: 'none' }}
+              aria-describedby="file-upload-helper-text"
+              value={filePath}
+              onChange={onChangeFile}
+            />
+            <InputLabel
+              style={{ display: 'inline' }}
+              htmlFor="upload-button-file"
+            >
+              <Button variant="contained" color="primary" component="span">
+                Upload
+              </Button>
+            </InputLabel>
+            <span style={{ marginLeft: '3em' }}>
+              {filename.length === 0 ? 'No file chosen' : filename}
+            </span>
 
-      {entityName === DocumentEnums.internalDocument.entityName && (
-        <FormGroup>
-          <Label for="format"> Format </Label>
-          <br />
-          <Select
-            id="format"
-            value={mimeType}
-            onChange={e => setMimeType(e.target.value)}
-          >
-            {mimeTypeOptions.map((option, i) => (
-              <MenuItem key={i} value={option}>
-                {option}
-              </MenuItem>
-            ))}
-          </Select>
+            {filename.length === 0 ? (
+              <FormHelperText
+                id="file-upload-helper-text"
+                error={filename.length === 0}
+              >
+                {filename.length === 0 ? 'Have to chose file' : ''}
+              </FormHelperText>
+            ) : (
+              <Label style={{ marginLeft: '1.5em' }} for="file-download">
+                <Link
+                  id="file-download"
+                  to={{ textDecoration: 'none' }}
+                  onClick={onDownloadFile}
+                >
+                  <HiDownload
+                    style={{ color: customTheme.palette.primary.main }}
+                    className={style.icons}
+                  />
+                </Link>
+              </Label>
+            )}
+          </>
+        )}
 
-          <br />
-          <br />
-          <CustomEditor
-            content={content}
-            setContent={setContent}
-            mimeType={mimeType}
+        {entityName === DocumentEnums.internalDocument.entityName && (
+          <>
+            {!isInEditingMode() && (
+              <>
+                <FormControl variant="outlined" style={{ minWidth: 250 }}>
+                  <InputLabel id="format-select-label">Format</InputLabel>
+                  <Select
+                    labelId="format-select-label"
+                    id="format-select"
+                    value={mimeType}
+                    onChange={e => setMimeType(e.target.value)}
+                    label="Format"
+                  >
+                    <MenuItem value="text/html">Rich (html)</MenuItem>
+                    <MenuItem value="text/markdown">
+                      Lightweight (Markdown)
+                    </MenuItem>
+                  </Select>
+                  <FormHelperText>
+                    Can't change after saving document
+                  </FormHelperText>
+                </FormControl>
+                <br />
+                <br />
+              </>
+            )}
+
+            <CustomEditor
+              content={content}
+              setContent={setContent}
+              mimeType={mimeType}
+            />
+            {content.length === 0 && (
+              <p style={{ color: customTheme.palette.error.main }}>
+                Document can't be empty
+              </p>
+            )}
+          </>
+        )}
+        <br />
+        <br />
+        <FormControlLabel
+          label="Is material"
+          control={
+            <Checkbox
+              checked={isMaterial}
+              onChange={e => setIsMaterial(e.target.checked)}
+              inputProps={{ 'aria-label': 'is material' }}
+            />
+          }
+        />
+        <br />
+
+        {isMaterial && (
+          <MaterialForm
+            courseId={courseId}
+            handleLoading={bool => setLoadingMaterialRelations(bool)}
+            statusHandler={responseStatus => setStatus(responseStatus)}
+            isAlternativeTo={isAlternativeTo}
+            setIsAlternativeTo={setIsAlternativeTo}
+            refersTo={refersTo}
+            setRefersTo={setRefersTo}
+            generalizes={generalizes}
+            setGeneralizes={setGeneralizes}
+            covers={covers}
+            setCovers={setCovers}
+            mentions={mentions}
+            setMentions={setMentions}
+            requires={requires}
+            setRequires={setRequires}
+            assumes={assumes}
+            setAssumes={setAssumes}
           />
-          {content.length === 0 && (
-            <p style={customErrorStyle}>Document can't be empty</p>
-          )}
-        </FormGroup>
-      )}
-      <br />
-
-      {status !== 200 && (
-        <Alert color="warning">
-          There has been a server error, try again please!
-        </Alert>
-      )}
-      <Button type="submit" color="success" >
-        Save editing
-      </Button>
-      <pre>{JSON.stringify(document, null, 2)}</pre>
-    </Form>
+        )}
+        {status !== 200 && (
+          <Alert color="warning">
+            There has been a server error, try again please!
+          </Alert>
+        )}
+        <Box
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Button
+            style={{ width: '30%', margin: 'auto' }}
+            size="large"
+            type="submit"
+            color="primary"
+            variant="contained"
+          >
+            Save document
+          </Button>
+        </Box>
+        {/* <pre>{JSON.stringify(document, null, 2)}</pre> */}
+      </Form>
+    </ThemeProvider>
   )
 }
 
 const mapStateToProps = state => {
-  const { authReducer, courseInstanceReducer } = state
+  const { authReducer, courseInstanceReducer, folderReducer } = state
   return {
     user: authReducer.user,
     courseInstance: courseInstanceReducer.courseInstance,
+    folder: { ...folderReducer },
   }
 }
 
