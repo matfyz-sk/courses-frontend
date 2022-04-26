58ca30c7600b1bb03f4eb5463051ca92fe172386
Rasto <raleu1@gmail.com>
2022-04-27 00:18:12 +0200

    request fix

--- src/pages/documents/functions/changeParent.js
+++ src/pages/documents/functions/changeParent.js
@@ -1,12 +1,20 @@
 import {
   axiosDeleteAttributeValueOfEntity,
-  axiosPartialEntityUpdate,
+  axiosPartialEntityUpdate, axiosUpdateEntity, getShortID, getShortType,
 } from '../../../helperFunctions'
+import { DocumentEnums } from "../enums/document-enums";
 
-export async function changeParent(fsObject, newParentId, oldParentId) {
+export async function changeParent(fsObject, newParentFullId, oldParentId) {
+  const entityName = getShortType(fsObject["@type"])
+  if (entityName === DocumentEnums.folder.entityName) {
+    const toUpdate = {
+      parent: newParentFullId
+    }
+    await axiosUpdateEntity(toUpdate, `folder/${getShortID(fsObject["@id"])}`)
+  }
   await axiosPartialEntityUpdate(
-    { content: fsObject['@id'] },
-    `folder/${newParentId}`
+    { content: [fsObject['@id']] },
+    `folder/${getShortID(newParentFullId)}`
   )
   await axiosDeleteAttributeValueOfEntity(
     { value: fsObject['@id'] },
